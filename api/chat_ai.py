c0f523961011d6985631b63d50ed4c9f6a1e143c
Seung-0208 <esybd02@naver.com>
2025-11-04 14:14:50 +0900

    refactor: 코드 분리 완료

---
+++ api/chat_ai.py
@@ -0,0 +1,51 @@
+import os
+from flask import jsonify
+from flask_restful import Resource
+from flask import request
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+class ChatAI(Resource):
+    def post(self):
+        messages = [{"role": "assistant", "content": '''
+                You are Helthy-Real's customer service referral bot.
+                What we offer is a service PlayStation that provides personal digital maintenance services.
+                It can cut into your personal health and recommend only healthy exercises as well as recipes and assets.
+                We try to obtain health information in a way that is neutral to the individual.
+                When responding, be courteous and polite to all users.
+                If you have any questions that do not provide a clear answer, please reply 'Please contact the administrator.'
+                Please reply using access since you are a Korean bot and most of your users are the newest
+                If the user wishes to cancel payment or reservation, they will be contacted by a counselor at 010-1234-1234.
+                Please indicate the link https://health.kdca.go.kr/healthinfo/ for additional health information.
+                All answers must be provided in Korean.
+                '''}]
+        while True:
+            content = request.json['message']
+            response = AIChatBot(content, messages)
+            messages = response['messages']
+            if response['status'] == 'SUCCESS':
+                answer = response['messages']
+                return jsonify({"answer": answer})
+            else:
+                print(messages)
+
+
+def AIChatBot(content, message):
+    if content == None or message == None:
+        return {'status': 'FAIL', 'messages': "필수 매개변수값이 없습니다"}
+    message.append({'role':'user','content':content})
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": "gpt-4o-mini",
+        "messages": message
+    }
+    response = requests.post(url, headers=headers, json=data)
+    answer = response.json()["choices"][0]['message']['content']
+    message.append({'role': 'assistant', 'content': answer})
+    return {'status': 'SUCCESS', 'messages': answer}
--- app.py
+++ app.py
@@ -2,10 +2,21 @@ import os
 from flask import Flask, Response
 from flask_restful import Api
 from flask_cors import CORS
+from api.summary import Summary
+from api.text_emotion_detect import TextEmotionDetection
+from api.create_image import CreateImage
+from api.chat_ai import ChatAI
 
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
-# api.add_resource(클래스, "도메인")
+api.add_resource(Summary, '/summaryAPI')
+api.add_resource(TextEmotionDetection, '/diary')
+api.add_resource(CreateImage, "/CreateIm")
+api.add_resource(ChatAI, "/ChatAI")
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
