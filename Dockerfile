fd4d73b2c55807ff68c7e47bbee1aa4b684be040
Seung-0208 <esybd02@naver.com>
2025-12-09 14:44:19 +0900

    feat: 영양소 분석 추가 및 설정 파일 수정

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 7003
 
-CMD ["gunicorn", "-b", "0.0.0.0:8000", "--workers", "4", "--worker-class", "gevent", "--worker-connections", "1000", "--timeout", "300", "--max-requests", "1000", "--max-requests-jitter", "100", "--preload", "--access-logfile", "-", "--error-logfile", "-", "--log-level", "info", "app:app"]
+CMD ["gunicorn", "-b", "0.0.0.0:7003", "--workers", "4", "--worker-class", "gevent", "--worker-connections", "1000", "--timeout", "300", "--max-requests", "1000", "--max-requests-jitter", "100", "--preload", "--access-logfile", "-", "--error-logfile", "-", "--log-level", "info", "app:app"]
--- app.py
+++ app.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
-api.add_resource(Summary, '/summaryAPI')
-api.add_resource(TextEmotionDetection, '/diary')
-api.add_resource(CreateImage, "/CreateIm")
-api.add_resource(ChatAI, "/ChatAI")
-api.add_resource(CalculateCalo, "/calculate-calo")
+api.add_resource(Summary, '/ai/conversation/summaryAPI')
+api.add_resource(TextEmotionDetection, '/ai/conversation/diary')
+api.add_resource(CreateImage, "/ai/conversation/CreateIm")
+api.add_resource(ChatAI, "/ai/conversation/ChatAI")
+api.add_resource(CalculateCalo, "/ai/conversation/calculate-calo")
 
 @app.route('/health')
 def health():
@@ -25,4 +25,4 @@ def health():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=7003)
