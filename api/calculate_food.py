39d13fd3e734ab612eb3c107f9b65ebb1e273e04
Seung-0208 <esybd02@naver.com>
2025-12-10 08:55:57 +0900

    로직 수정

--- api/calculate_food.py
+++ api/calculate_food.py
@@ -62,5 +62,5 @@ class CalculateCalo(Resource):
             #     data = ast.literal_eval(answer)
             #     return data, 200
         except Exception as e:
-            print(f'오류 발생: {str(e)}')
+            print(f'오류 발생: {str(e)}') #test
             return {'error': str(e)}, 500
